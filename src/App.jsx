d15569264d67791f72021e1409466217ce1f4294
Raminka <raminka13@hotmail.com>
2022-08-04 20:20:18 -0500

    Refactor Calculate.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
 import Calculator from './components/Calculator';
 import Navigation from './components/Nav';
 
-class App extends React.PureComponent {
-  render() {
-    return (
-      <div>
-        <header><Navigation /></header>
-        <main className="main-container">
-          <Calculator />
-        </main>
-        <footer className="footer-container" />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <header><Navigation /></header>
+      <main className="main-container">
+        <Calculator />
+      </main>
+      <footer className="footer-container" />
+    </div>
+  );
 }
 
 export default App;
